e55f646a449db77ca0971ea802ec1df1963bacc5
bianca-efs <bianca.silva.emailprofissional@gmail.com>
2026-02-19 22:02:55 -0300

    incluindo comentários

--- E.Dados01.02/E.Dados01.e02/src/controller/Metodos.java
+++ E.Dados01.02/E.Dados01.e02/src/controller/Metodos.java
@@ -3,9 +3,21 @@ package controller;
 public class Metodos {
 
 	public static int mnNumero (int vet [ ], int mn, int length ) {
+
+		// Condição de parada:
+		// A recursão para quando o índice do vetor chega antes da primeira posição (length - 1 < 0),
+		// ou seja, quando todos os elementos já foram verificados.
+		// Nesse momento, o menor valor encontrado (mn) é retornado.
+
 		if (length - 1 < 0) {
 			return mn;
 		}
+		
+		// Relação de chamada (passo recursivo):
+		// Compara o valor da posição atual do vetor com o menor valor encontrado até o momento (mn).
+		// Se o valor atual for menor, ele passa a ser o novo menor.
+		// Em seguida, a função chama ela mesma, diminuindo o tamanho (length - 1),
+		// para continuar verificando as posições anteriores do vetor.
 		
 		else {
 			if (vet [length - 1] < mn) {
